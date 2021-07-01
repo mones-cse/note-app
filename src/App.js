8875cd6df658721d571698edd1456dbd0869286e
mones <mones.cse@gmail.com>
2021-07-01 16:03:54 +0600

    added react-icon react-color toster validator modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
             <div className={"text-center bg-dark text-light min-vh-100"}>
-                Hello world!!!
+                <h3>Hello world!!!</h3>
             </div>
         </div>
     );
